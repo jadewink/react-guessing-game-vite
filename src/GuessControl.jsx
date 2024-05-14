3818dbc01ef444c1046feab1931d62fcbe98f19e
Jordyn Jones <minicakes@Minicakes-MacBook-Pro.local>
2024-05-14 00:40:34 -0700

    Convert GuessControl, mostly working, not sure what to do with onGuessprop

--- src/GuessControl.jsx
+++ src/GuessControl.jsx
@@ -1,47 +1,79 @@
 import React, { Component } from "react";
 import Button from "./Button";
+import { useState, useEffect } from 'react';
 
-class GuessControl extends Component {
-  constructor(props) {
-    super(props);
+function GuessControl(onGuess) { 
 
-    this.state = {
-      currentGuess: "",
-    };
+  const [currentGuess, setCurrentGuess] = useState(""); 
 
-    /**
-     * These lines are required to make the methods/functions declared on this
-     *  class have the correct `this` object when they run.
-     */
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.onSubmitGuess = this.onSubmitGuess.bind(this);
+
+  function handleInputChange() {
+    let currentGuess = event.target.value;
+    // console.log(currentGuess);
+    setCurrentGuess(currentGuess);
   }
 
-  handleInputChange(event) {
-    this.setState({ currentGuess: event.target.value });
+  function onSubmitGuess(onGuess) {
+    console.log((Number(currentGuess)));
+    setCurrentGuess("");
+    console.log(onGuess.setCurrentGuess);
+    // this.props.onGuess(Number(this.state.currentGuess));
+
   }
 
-  onSubmitGuess() {
-    // Since the values from an HTML input are strings by default,
-    //  convert to a number for the returned guess value
-    //  by passing in the string to the Number function.
-    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-    this.props.onGuess(Number(this.state.currentGuess));
-    this.setState({ currentGuess: "" });
-  }
-
-  render() {
-    return (
-      <div>
-        <input
-          type="number"
-          value={this.state.currentGuess}
-          onChange={this.handleInputChange}
-        />
-        <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <input
+        type="number"
+        // value={currentGuess}
+        onChange={handleInputChange}
+      />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
+  );
 }
+
+// class GuessControlOld extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       currentGuess: "",
+//     };
+
+//     /**
+//      * These lines are required to make the methods/functions declared on this
+//      *  class have the correct `this` object when they run.
+//      */
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.onSubmitGuess = this.onSubmitGuess.bind(this);
+//   }
+
+//   handleInputChange(event) {
+//     this.setState({ currentGuess: event.target.value });
+//   }
+
+//   onSubmitGuess() {
+//     // Since the values from an HTML input are strings by default,
+//     //  convert to a number for the returned guess value
+//     //  by passing in the string to the Number function.
+//     // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+//     this.props.onGuess(Number(this.state.currentGuess));
+//     this.setState({ currentGuess: "" });
+//   }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <input
+  //         type="number"
+  //         value={this.state.currentGuess}
+  //         onChange={this.handleInputChange}
+  //       />
+  //       <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
+  //     </div>
+  //   );
+  // }
+// }
 
 export default GuessControl;
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+createRoot(document.getElementById('root')).render(<App />)
